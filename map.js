fb6bd623b6071e94c052126c0595349711bc8871
Jeongho <cuzzzu1318@naver.com>
2021-07-11 20:26:41 +0900

    a

--- map.js
+++ map.js
@@ -1,4 +1,3 @@
-<script>
 
   var mapContainer = document.getElementById('map'), // 지도를 표시할 div
       mapOption = {
@@ -8,5 +7,3 @@
 
   // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
   var map = new kakao.maps.Map(mapContainer, mapOption);
-
-</script>
